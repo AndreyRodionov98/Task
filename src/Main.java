9d4975f3805b20974240c3836ad7f8d910497b37
Andrey_Rodionov <andrey_36rus@mail.ru>
2022-11-07 13:00:35 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+   public static void main(String[] args){
+     String numbers="0123456789";
+     final int number= numbers.length()/2;
+     String[] parts={
+             numbers.substring(0,number),
+             numbers.substring(number),
+     };
+       System.out.println(Arrays.toString(parts));
+
+
 
     }
 }
